1ee0a186645175b5001e143986bc60517d4a9d9b
Siterizer <siterizer@gmail.com>
2019-07-19 19:49:29 +0200

    1st week of working

---
+++ src/main/java/controllers/AddPersonController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sample.People;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.nio.charset.IllegalCharsetNameException;
+
+public class AddPersonController {
+
+    @FXML
+    private TextArea firstName;
+
+    @FXML
+    private TextArea lastName;
+
+    @FXML
+    void handleAdd(ActionEvent event) {
+        try{
+            if(firstName.getText().length() < 3){
+                throw new IllegalCharsetNameException("FirstNameLengthException");
+            }
+            if(!firstName.getText().matches("[a-zA-Z]+")){
+                throw new IllegalCharsetNameException("FirstNameCharacterException");
+            }
+            if(lastName.getText().length() < 3){
+                throw new IllegalCharsetNameException("LastNameLengthException");
+            }
+            if(!lastName.getText().matches("[a-zA-Z]+")){
+                throw new IllegalCharsetNameException("LastNameCharacterException");
+            }
+            new People(firstName.getText(), lastName.getText());
+            message("Dodawanie zakonczone sukcesem");
+        }
+
+        catch(IllegalCharsetNameException e){
+            System.out.println(e.getCharsetName());
+            message("Dodawanie nie powiodlo sie");
+        }
+
+    }
+    private void message(String message){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MessageBox.fxml"));
+            Parent root1 = fxmlLoader.load();
+            MessageBoxController controller = fxmlLoader.getController();
+            controller.setMessage(message);
+            Stage stage = new Stage();
+            stage.setTitle("Wiadomosc");
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+            System.out.println(People.funkcja());
+        } catch (IOException e) {
+            System.out.println("Nie mozna otworzyc okna 'Wiadomosc'");
+        }
+    }
+
+}
---
+++ src/main/java/sample/People.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.Vector;
+
+public class People {
+
+    private String imie;
+    private String nazwisko;
+    private static Vector<People> ludzie = new Vector<People>();
+
+    public People(String imie, String nazwisko){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        ludzie.add(this);
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+    public static Vector<People> funkcja(){
+        return ludzie;
+    }
+}
+
