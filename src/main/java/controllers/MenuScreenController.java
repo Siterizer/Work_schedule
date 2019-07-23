c29327326991bcc706e64ffb0b8a4b67755a3789
Dawid Koziej <januszakahipis@wp.pl>
2019-07-23 13:18:00 +0200

    add NewSchedule fxml/java and add it to MenuScreen, update MenuScreen

--- src/main/java/controllers/MenuScreenController.java
+++ src/main/java/controllers/MenuScreenController.java
@@ -46,4 +46,26 @@ public class MenuScreenController {
             System.out.println("Nie mozna otworzyc okna 'Usun opiekuna'");
         }
     }
+
+    @FXML
+    void handleOpiekunowieNowyGrafik(ActionEvent event) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/NewSchedule.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Nowy Grafik");
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Nie mozna otworzyc okna 'Nowy Grafik'");
+        }
+    }
+
+    @FXML
+    void handleOpiekunowieGeneruj(ActionEvent event) {
+
+    }
 }
--- src/main/java/controllers/NewScheduleController.java
+++ src/main/java/controllers/NewScheduleController.java
@@ -1,11 +1,20 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class NewScheduleController {
 
     @FXML
-    public void initialize() {
+    private ChoiceBox<String> monthChoiceBox;
 
+    @FXML
+    public void initialize() {
+        monthChoiceBox.getItems().addAll(
+                "Styczeń", "Luty", "Marzec",
+                "Kwiecień", "Maj", "Czerwiec",
+                "Lipiec", "Sierpień", "Wrzesień",
+                "Październik", "Listopad", "Grudzień");
+        monthChoiceBox.setValue("Styczeń");
     }
 }
