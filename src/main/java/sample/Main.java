c46c81b2315eda1ad388de9a45e38cc1fbe39fa6
Dawid Koziej <januszakahipis@wp.pl>
2019-07-23 12:03:17 +0200

    remove usless import

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
