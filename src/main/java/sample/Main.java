8820133a8119a572af0e6563a442ee2aa7936cef
Dawid Koziej <januszakahipis@wp.pl>
2019-07-23 12:02:32 +0200

    temporary solution to method

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,10 +2,12 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -14,9 +16,21 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MenuScreen.fxml"));
         primaryStage.setTitle("Apka");
         primaryStage.setScene(new Scene(root));
+        //chwilowe rozwiazanie
+        setScreenDimensions(primaryStage);
+        //
         primaryStage.setMaximized(true);
         primaryStage.show();
 
+    }
+    private void setScreenDimensions(Stage stage){
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
     }
 
 
