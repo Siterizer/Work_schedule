259c378904b41f6ea61c404771b8de25a54b4de6
Dawid Koziej <januszakahipis@wp.pl>
2019-07-23 15:23:59 +0200

    update AddPersonController to Adam version

--- src/main/java/controllers/AddPersonController.java
+++ src/main/java/controllers/AddPersonController.java
@@ -5,22 +5,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import sample.People;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.nio.charset.IllegalCharsetNameException;
 
 public class AddPersonController {
 
     @FXML
-    private TextArea firstName;
+    private TextField firstName;
 
     @FXML
-    private TextArea lastName;
+    private TextField lastName;
 
     @FXML
     void handleAdd(ActionEvent event) {
