0192a36a24312eca6c7f27a2713bd48020149918
Dawid Koziej <januszakahipis@wp.pl>
2019-07-25 17:02:20 +0200

    make live showPeople

--- src/main/java/controllers/AddPersonController.java
+++ src/main/java/controllers/AddPersonController.java
@@ -67,7 +67,7 @@ public class AddPersonController {
             stage.setResizable(false);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.show();
-            System.out.println(People.funkcja());
+            People.showPeople();
         } catch (IOException e) {
             System.out.println("Nie mozna otworzyc okna 'Wiadomosc'");
         }
