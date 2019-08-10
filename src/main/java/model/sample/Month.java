47a4fbf801891e504165c533aa66ee607220fa64
Dawid Koziej <januszakahipis@wp.pl>
2019-08-10 12:17:27 +0200

    add try catch to addToVector

--- src/main/java/model/sample/Month.java
+++ src/main/java/model/sample/Month.java
@@ -8,15 +8,19 @@ public class Month {
     private int noMonth;
     private Vector<Day> daysVector;
 
-    public Month(int noDays, int noMonth){
+    public Month(int noDays, int noMonth) {
         this.noDays = noDays;
         this.noMonth = noMonth;
         daysVector = new Vector<Day>();
     }
 
-    public void addToVector(Day newDay){
-        if(daysVector.size() <= this.noDays){
+    public void addToVector(Day newDay) {
+        try {
+            if (daysVector.size() > this.noDays)
+                throw new Exception("wielkosc wektora > ilosc dni w miesiacu");
             daysVector.add(newDay);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
