d03bbfd57e959213f7bb6c6e1220b5f33868d03c
Dawid Koziej <januszakahipis@wp.pl>
2019-08-10 23:52:55 +0200

    add temp functions

--- src/main/java/model/sample/Day.java
+++ src/main/java/model/sample/Day.java
@@ -1,19 +1,23 @@
 package model.sample;
 
 public class Day {
-    private boolean holiday;
+    private int holiday;
     private int nuDayInMonth;
 
-    public Day(int nuDayInMonth, boolean holiday) {
+    public Day(int nuDayInMonth, int holiday) {
         this.nuDayInMonth = nuDayInMonth;
         this.holiday = holiday;
     }
 
-    public boolean isHoliday() {
+    public int isHoliday() {
         return holiday;
     }
 
     public int getNuDayInMonth() {
         return nuDayInMonth;
+    }
+
+    public void showDay(){
+        System.out.printf("nuDayInMonth=%d, holiday=%d",this.nuDayInMonth, this.holiday);
     }
 }
