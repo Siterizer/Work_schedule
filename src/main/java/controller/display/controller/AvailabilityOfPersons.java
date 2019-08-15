58ed6b0194408054f6e870aeee4234d114867872
Dawid Koziej <januszakahipis@wp.pl>
2019-08-15 13:24:53 +0200

    delete useless imports

--- src/main/java/controller/display/controller/AvailabilityOfPersons.java
+++ src/main/java/controller/display/controller/AvailabilityOfPersons.java
@@ -1,13 +1,8 @@
 package controller.display.controller;
 
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 public class AvailabilityOfPersons {
 
-    public static void display(){
+    public static void display() {
         DisplayTemplate template = new DisplayTemplate("/fxml/AvailabilityOfPersons.fxml", "Zmien dyspozycyjnosc");
 
         template.getStage().setResizable(true);
