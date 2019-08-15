eaee77d1578b183097c4b9173e947d865f64b92d
Dawid Koziej <januszakahipis@wp.pl>
2019-08-15 13:22:29 +0200

    good window issue: stage cant be setResizable(fasle)

--- src/main/java/controller/display/controller/AvailabilityOfPersons.java
+++ src/main/java/controller/display/controller/AvailabilityOfPersons.java
@@ -1,9 +1,18 @@
 package controller.display.controller;
 
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 public class AvailabilityOfPersons {
 
     public static void display(){
         DisplayTemplate template = new DisplayTemplate("/fxml/AvailabilityOfPersons.fxml", "Zmien dyspozycyjnosc");
+
+        template.getStage().setResizable(true);
+        template.getStage().setMaximized(true);
+
         template.getStage().show();
     }
 }
